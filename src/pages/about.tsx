0a701ee2079c2c4669936d5f86ea1c327c89f6a6
hawkeyeinsights <ikram.hawk@gmail.com>
2025-06-25 15:21:22 +0100

    Feature: Add standalone About page with company details 2

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,20 +2,22 @@ import { Meta } from '../layout/Meta';
 import { Nav } from '../navigation/Nav';
 import { About } from '../templates/About';
 import { Footer } from '../templates/Footer';
-import { AppConfig } from '../utils/AppConfig';
 
 const AboutPage = () => (
   <div className="bg-brand-dark text-gray-600 antialiased">
     <Meta
-      title={`About | ${AppConfig.title}`}
+      title="About - Cybernetic Hawk"
       description="Learn more about Cybernetic Hawk."
     />
-
-    <div className="px-4 sm:px-8">
+    <header className="px-4 sm:px-6 lg:px-8">
       <Nav />
+    </header>
+
+    <main className="px-4 py-20 text-center sm:px-6 lg:px-8">
       <About />
-      <Footer />
-    </div>
+    </main>
+
+    <Footer />
   </div>
 );
 
